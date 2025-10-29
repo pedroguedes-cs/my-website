a952b99a4d19ce089e96af81e433ecb3a731595c
Pedro <pgdtsouza@gmail.com>
2025-10-29 12:18:36 -0300

    Implement the new slider model

--- scripts/projects_preview.js
+++ scripts/projects_preview.js
@@ -1,13 +1,12 @@
 import { projectsData } from "../data/projects_data.js";
 
 // HTML SLIDER CONTENT
-// HTML elements
-let projectsPreviewContainer = document.querySelector('.js-projects-preview-container');
+let projectsPreviewContainer = document.querySelector('.js-projects-preview-slider');
 
 
 function renderProjectsPreview()
 {
-    let projectsPreviewHTML = '';
+    let projectsPreviewHTML = '<div class="projects-preview-slider-content js-projects-preview-slider-content">';
     let cardsCounter = 0;
 
     for (let category = 0; category < projectsData.length; category++)
@@ -30,11 +29,13 @@ function renderProjectsPreview()
         }
     }
 
+    projectsPreviewHTML += '</div>'
+
     if (cardsCounter > 0)
     {
-        projectsPreviewHTML += '<button class="control-button js-control-button previous-button"><svg class="chevronIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z"/></svg></button>';
+        projectsPreviewHTML += '<button class="control-button js-previous-button previous-button"><svg class="chevronIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z"/></svg></button>';
 
-        projectsPreviewHTML += '<button class="control-button js-control-button next-button"><svg class="chevronIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z"/></svg></button>';   
+        projectsPreviewHTML += '<button class="control-button js-next-button next-button"><svg class="chevronIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z"/></svg></button>';   
     }
 
     projectsPreviewContainer.innerHTML = projectsPreviewHTML;
@@ -43,11 +44,11 @@ function renderProjectsPreview()
 renderProjectsPreview();
 
 
-// SLIDER RUNNING
-// HTML elements
+// SLIDER 
 const sliderCardsArray = document.querySelectorAll('.project-preview-card');
-let nextButton = document.querySelector('.next-button');
-let previousButton = document.querySelector('.previous-button');
+const sliderContent = document.querySelector('.js-projects-preview-slider-content');
+const nextButton = document.querySelector('.js-next-button');
+const previousButton = document.querySelector('.js-previous-button');
 let timeoutId = null;
 let intervalId = null;
 
@@ -56,25 +57,18 @@ let index = 0;
 nextButton.addEventListener('click', () => {
     clearTimeout(timeoutId);
     clearInterval(intervalId);
-    nextImage();
-    timeoutId = setTimeout(() => {intervalId = setInterval(() => {nextImage()}, 6000)}, 8000);
+    nextSlide();
+    timeoutId = setTimeout(() => {intervalId = setInterval(() => {nextSlide()}, 6000)}, 8000);
 })
 
 previousButton.addEventListener('click', () => {
     clearTimeout(timeoutId);
     clearInterval(intervalId);
-    previousImage();
-    timeoutId = setTimeout(() => {intervalId = setInterval(nextImage, 6000)}, 8000);
+    previousSlide();
+    timeoutId = setTimeout(() => {intervalId = setInterval(nextSlide, 6000)}, 8000);
 })
 
-function InitializeImages()
-{
-    sliderCardsArray[index].classList.add('project-preview-card-show');
-    intervalId = setInterval(nextImage, 8000)
-    showImage();
-}
-
-function nextImage()
+function nextSlide()
 {
     index++;
 
@@ -83,10 +77,10 @@ function nextImage()
         index = 0;
     }
 
-    showImage();
+    showSlide();
 }
 
-function previousImage()
+function previousSlide()
 {
     index--;
 
@@ -95,13 +89,12 @@ function previousImage()
         index = sliderCardsArray.length - 1;
     }
 
-    showImage();
+    showSlide();
 }
 
-function showImage()
+function showSlide()
 {
-    sliderCardsArray.forEach((card) => {card.classList.remove('project-preview-card-show')});
-    sliderCardsArray[index].classList.add('project-preview-card-show');
+    sliderContent.style.transform = `translateX(-${index * 100}%)`;
 }
 
-InitializeImages();
+showSlide();
