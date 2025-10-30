3b0ec8045f0998102f48b135fc326fbcf6e1f37e
Pedro <pgdtsouza@gmail.com>
2025-10-30 12:26:35 -0300

    Saving light mode correctly

--- scripts/light_mode.js
+++ scripts/light_mode.js
@@ -1,9 +1,45 @@
-const lightModeButtons = document.querySelectorAll('.light-mode-button');
+let mode = JSON.parse(localStorage.getItem('lightMode'));
+if (mode === null)
+{
+    mode = 1;
+}
 
+const lightModeButtons = document.querySelectorAll('.light-mode-button');
 lightModeButtons.forEach((button) => {button.addEventListener('click', changeLightMode)})
+
+function applyMode()
+{
+    if (mode === 1)
+    {
+       document.body.classList.remove('dark-mode');
+    }
+    else
+    {
+        document.body.classList.add('dark-mode');
+    }
+}
 
 function changeLightMode()
 {
     console.log('Changing color');
-    document.body.classList.toggle('dark-mode');
+
+    if (mode === 1)
+    {
+        mode = 0;
+    }
+    else
+    {
+        mode = 1;
+    }
+
+    applyMode();
+
+    saveModeToStorage();
 }
+
+function saveModeToStorage()
+{
+    localStorage.setItem('lightMode', JSON.stringify(mode));
+}
+
+applyMode();
